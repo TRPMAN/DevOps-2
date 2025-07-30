a830b9820297215bada86a7bde61eff9fbfa756d
TRPMAN <trp.man08@gmail.com>
2025-07-30 17:13:19 +0700

    Update 02_ec2.tf

--- 02_ec2.tf
+++ 02_ec2.tf
@@ -1,8 +1,12 @@
 # Resource: EC2 Instance
 resource "aws_instance" "ec2_1" {
-  ami = "ami-08a6efd148b1f7504"
-  instance_type = "t3.micro"
+  # .id come from attribute on document
+  ami = data.aws_ami.ami.id
+  instance_type = var.instance_type
   user_data = file("${path.module}/app_install.sh")
+  key_name = var.Instance_keypair
+  # .id come from attribute on document
+  vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
   tags = {
     "name" = "ec2 demo"
   }

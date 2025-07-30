d79175427014ce89697fcfe26d9e923f12c25bae
TRPMAN <trp.man08@gmail.com>
2025-07-30 17:08:19 +0700

    Create 05_ami_datasources.tf

---
+++ 05_ami_datasources.tf
@@ -0,0 +1,20 @@
+# Get latest AMI ID for Amazon Linux
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-*-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}

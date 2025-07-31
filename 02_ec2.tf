2f04a4eefb1d206e2e1736223fef320b743efa63
TRPMAN <trp.man08@gmail.com>
2025-07-31 20:21:21 +0700

    update availability_zone

--- 02_ec2.tf
+++ 02_ec2.tf
@@ -15,14 +15,14 @@ resource "aws_instance" "ec2_tf_basic" {
   vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
 
   # for_each receive only set of string or map !!! not list
-  for_each = toset(aws_availability_zones.ec2_zone.names)
+  for_each = toset(data.aws_availability_zones.ec2_zone.names)
+  availability_zone = each.key
+
   # create the same instance 2 times
   # count = 2
   tags = {
     "Name" = "EC2Demo-${each.key}"
   }
-
-
 }
 
 data "aws_availability_zones" "ec2_zone" {
@@ -30,4 +30,6 @@ data "aws_availability_zones" "ec2_zone" {
     name   = "opt-in-status"
     values = ["opt-in-not-required"]
   }
+
+  exclude_names = ["us-east-1e"]
 }

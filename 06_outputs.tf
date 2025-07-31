dbde4c3d86465047035c7ec5a1e2298661e31d17
TRPMAN <trp.man08@gmail.com>
2025-07-31 18:17:23 +0700

    using loop on output

--- 06_outputs.tf
+++ 06_outputs.tf
@@ -3,13 +3,43 @@
 # EC2 Public IP
 output "instance_publicip" {
   description = "EC2 Instance Public IP"
+  # .public_ip and .public_dns come from attribute on document
   value = aws_instance.ec2_tf_basic.public_ip
 }
 
 # EC2 Public DNS
 output "instance_publicdns" {
   description = "EC2 Instance Public DNS"
+  # .public_ip and .public_dns come from attribute on document
   value = aws_instance.ec2_tf_basic.public_dns
 }
 
-# .public_ip and .public_dns come from attribute on document
+# Output - Loop List
+output "output_list" {
+  description = "For loop with list"
+  value = [for output in aws_instance.ec2_tf_basic: output.public_dns]
+}
+
+# Output - Loop Map
+output "output_map" {
+  description = "For loop with map"                   # key            value
+  value = {for output in aws_instance.ec2_tf_basic: output.id => output.public_dns}
+}
+
+# Output - Loop Map Advanced
+output "output_map_ad" {
+  description = "For loop with map advanced"              
+  value = {for c, output in aws_instance.aws_instance.ec2_tf_basic: c => instance.public_dns}
+}
+
+# Output - Legacy Splat Operator
+output "legacy_splat_output" {
+  description = "Legacy Splat Operator"              
+  value = aws_instance.ec2_tf_basic.*.public_dns
+}
+
+# Output - Latest Generalized Splat Operator
+output "legacy_splat_output" {
+  description = "Generalized Splat Operator"              
+  value = aws_instance.ec2_tf_basic[*].public_dns
+}

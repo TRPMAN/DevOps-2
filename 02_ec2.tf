a4f282799cb75e794b0513498bfaad377ced6b4f
TRPMAN <trp.man08@gmail.com>
2025-07-31 17:41:20 +0700

    using var list and map

--- 02_ec2.tf
+++ 02_ec2.tf
@@ -2,9 +2,15 @@
 resource "aws_instance" "ec2_tf_basic" {
   # .id come from attribute on document
   ami = data.aws_ami.ami.id
-  instance_type = var.instance_type
+
+  # instance_type = var.instance_type
+  # Using List: instance_type = var.instance_type_list[1]
+  # Using Map:
+  instance_type = var.instance_type_map["mi"]
+
   user_data = file("${path.module}/app_install.sh")
   key_name = var.Instance_keypair
+
   # .id come from attribute on document
   vpc_security_group_ids = [aws_security_group.vpc_ssh.id, aws_security_group.vpc_web.id]
   tags = {
--- 03_vars.tf
+++ 03_vars.tf
@@ -14,6 +14,24 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
+# AWS EC2 Instance Type - List
+variable "instance_type_list" {
+  description = "EC2 Instance type in List"
+  type = list(string)
+  default = ["t3.micro","t3.small","t3.large"]
+}
+
+# AWS EC2 Instance Type - Map
+variable "instance_type_map" {
+  description = "EC2 Instance type in Map"
+  type = map(string)
+  default = {
+    "mi" = "t3.micro"
+    "sm" = "t3.small"
+    "lg" = "t3.large"
+  }
+}
+
 # AWS EC2 Key Pair
 variable "Instance_keypair" {
   description = "Key Pair for EC2"

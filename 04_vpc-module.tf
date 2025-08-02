421f959834f1f4e588cbaee5c04286ebe2790b82
TRPMAN <trp.man08@gmail.com>
2025-08-02 16:52:07 +0700

    update datasource for azs

--- 04_vpc-module.tf
+++ 04_vpc-module.tf
@@ -6,7 +6,8 @@ module "vpc_1" {
   # VPC Basic Details
   name            = "${local.name}-${var.vpc_name}"
   cidr            = var.vpc_cidr_block
-  azs             = var.vpc_availability_zones
+  #azs             = var.vpc_availability_zones
+  azs             = data.aws_availability_zones.azs.names
   private_subnets = var.vpc_private_subnets
   public_subnets  = var.vpc_public_subnets
 
---
+++ 05_datasources.tf
@@ -0,0 +1,4 @@
+# AWS Availability Zones
+data "aws_availability_zones" "azs" {
+  #state = "available"
+}

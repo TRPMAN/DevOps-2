d039da675ae935cd55930362b276c79e0158c0d4
TRPMAN <trp.man08@gmail.com>
2025-08-08 17:21:10 +0700

    create k8smanifest using k8s provider

---
+++ 02_Kubernetes/05_kube-nlb-service.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_service_v1" "nlb_service" {
+  metadata {
+    name = "myapp1-nlb-service"
+    annotations = { 
+      "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb" 
+    }
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment_v1.myapp1.spec.0.selector.0.match_labels.app
+    }
+    port {
+      name        = "http"
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
